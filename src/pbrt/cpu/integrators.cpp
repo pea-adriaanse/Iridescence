pea-adriaanse/Iridescence

commit b459ba889bf727ce392532645213125b567d7fb7

Update from book source.
The CPU integrators no longer write out WIP images during rendering by
default, but this functionality can be enabled using the new
--write-partial-images command-line flag.

src/pbrt/cpu/integrators.cpp
@@ -160,7 +160,7 @@ void ImageTileIntegrator::Render() {
     if (Options->recordPixelStatistics)
         StatsEnablePixelStats(pixelBounds,
                               RemoveExtension(camera.GetFilm().GetFilename()));
-    // Handle MSE referene image, if provided
+    // Handle MSE reference image, if provided
     pstd::optional<Image> referenceImage;
     FILE *mseOutFile = nullptr;
     if (!Options->mseReferenceImage.empty()) {
@@ -242,22 +242,27 @@ void ImageTileIntegrator::Render() {
         if (!referenceImage)
             nextWaveSize = std::min(2 * nextWaveSize, 64);
 
-        // Write current image to disk
-        LOG_VERBOSE("Writing image with spp = %d", waveStart);
-        ImageMetadata metadata;
-        metadata.renderTimeSeconds = progress.ElapsedSeconds();
-        metadata.samplesPerPixel = waveStart;
-        if (referenceImage) {
-            ImageMetadata filmMetadata;
-            Image filmImage = camera.GetFilm().GetImage(&filmMetadata, 1.f / waveStart);
-            ImageChannelValues mse =
-                filmImage.MSE(filmImage.AllChannelsDesc(), *referenceImage);
-            fprintf(mseOutFile, "%d, %.9g\n", waveStart, mse.Average());
-            metadata.MSE = mse.Average();
-            fflush(mseOutFile);
+        // Optionally write current image to disk
+        if (Options->writePartialImages || referenceImage) {
+            LOG_VERBOSE("Writing image with spp = %d", waveStart);
+            ImageMetadata metadata;
+            metadata.renderTimeSeconds = progress.ElapsedSeconds();
+            metadata.samplesPerPixel = waveStart;
+            if (referenceImage) {
+                ImageMetadata filmMetadata;
+                Image filmImage =
+                    camera.GetFilm().GetImage(&filmMetadata, 1.f / waveStart);
+                ImageChannelValues mse =
+                    filmImage.MSE(filmImage.AllChannelsDesc(), *referenceImage);
+                fprintf(mseOutFile, "%d, %.9g\n", waveStart, mse.Average());
+                metadata.MSE = mse.Average();
+                fflush(mseOutFile);
+            }
+            if (Options->writePartialImages) {
+                camera.InitMetadata(&metadata);
+                camera.GetFilm().WriteImage(metadata, 1.0f / waveStart);
+            }
         }
-        camera.InitMetadata(&metadata);
-        camera.GetFilm().WriteImage(metadata, 1.0f / waveStart);
     }
 
     if (mseOutFile)
@@ -268,7 +273,7 @@ void ImageTileIntegrator::Render() {
 }
 
 // RayIntegrator Method Definitions
-void RayIntegrator::EvaluatePixelSample(const Point2i &pPixel, int sampleIndex,
+void RayIntegrator::EvaluatePixelSample(Point2i pPixel, int sampleIndex,
                                         SamplerHandle sampler,
                                         ScratchBuffer &scratchBuffer) {
     // Initialize _CameraSample_ for current sample
@@ -287,10 +292,9 @@ void RayIntegrator::EvaluatePixelSample(const Point2i &pPixel, int sampleIndex,
     pstd::optional<CameraRayDifferential> cameraRay =
         camera.GenerateRayDifferential(cameraSample, lambda);
 
+    // Trace _cameraRay_ if valid
     SampledSpectrum L(0.);
     VisibleSurface visibleSurface;
-    bool initializeVisibleSurface = camera.GetFilm().UsesVisibleSurface();
-    // Trace _cameraRay_ if valid
     if (cameraRay) {
         // Double check that the ray's direction is normalized.
         DCHECK_GT(Length(cameraRay->ray.d), .999f);
@@ -303,6 +307,7 @@ void RayIntegrator::EvaluatePixelSample(const Point2i &pPixel, int sampleIndex,
 
         ++nCameraRays;
         // Evaluate radiance along camera ray
+        bool initializeVisibleSurface = camera.GetFilm().UsesVisibleSurface();
         L = cameraRay->weight * Li(cameraRay->ray, lambda, sampler, scratchBuffer,
                                    initializeVisibleSurface ? &visibleSurface : nullptr);
 
@@ -558,7 +563,7 @@ LightPathIntegrator::LightPathIntegrator(int maxDepth, CameraHandle camera,
     lightSampler = std::make_unique<PowerLightSampler>(lights, Allocator());
 }
 
-void LightPathIntegrator::EvaluatePixelSample(const Point2i &pPixel, int sampleIndex,
+void LightPathIntegrator::EvaluatePixelSample(Point2i pPixel, int sampleIndex,
                                               SamplerHandle sampler,
                                               ScratchBuffer &scratchBuffer) {
     // Consume first two dimensions from sampler

src/pbrt/cmd/pbrt.cpp
@@ -81,6 +81,8 @@ Rendering options:
   --seed <n>                   Set random number generator seed. Default: 0.
   --spp <n>                    Override number of pixel samples specified in scene
                                description file.
+  --write-partial-images       Periodically write the current image to disk, rather
+                               than waiting for the end of rendering. Default: disabled.
 
 Logging options:
   --log-level <level>          Log messages at or above this level, where <level>
@@ -177,6 +179,8 @@ int main(int argc, char *argv[]) {
             ParseArg(&argv, "seed", &options.seed, onError) ||
             ParseArg(&argv, "spp", &options.pixelSamples, onError) ||
             ParseArg(&argv, "toply", &toPly, onError) ||
+            ParseArg(&argv, "write-partial-images", &options.writePartialImages,
+                     onError) ||
             ParseArg(&argv, "upgrade", &options.upgrade, onError)) {
             // success
         } else if ((strcmp(*argv, "--help") == 0) || (strcmp(*argv, "-help") == 0) ||

src/pbrt/cpu/integrators.h
@@ -62,15 +62,15 @@ class Integrator {
                        const SampledWavelengths &lambda, RNG &rng) const;
 
     // Integrator Public Members
-    std::vector<LightHandle> lights;
     PrimitiveHandle aggregate;
+    std::vector<LightHandle> lights;
     std::vector<LightHandle> infiniteLights;
 
   protected:
     // Integrator Private Methods
     Integrator(PrimitiveHandle aggregate, std::vector<LightHandle> lights)
-        : lights(lights), aggregate(aggregate) {
-        // Integrator Constructor Implementation
+        : aggregate(aggregate), lights(lights) {
+        // Integrator constructor implementation
         if (aggregate)
             sceneBounds = aggregate.Bounds();
 
@@ -95,7 +95,7 @@ class ImageTileIntegrator : public Integrator {
 
     void Render();
 
-    virtual void EvaluatePixelSample(const Point2i &pPixel, int sampleIndex,
+    virtual void EvaluatePixelSample(Point2i pPixel, int sampleIndex,
                                      SamplerHandle sampler,
                                      ScratchBuffer &scratchBuffer) = 0;
 
@@ -113,8 +113,8 @@ class RayIntegrator : public ImageTileIntegrator {
                   std::vector<LightHandle> lights)
         : ImageTileIntegrator(camera, sampler, aggregate, lights) {}
 
-    void EvaluatePixelSample(const Point2i &pPixel, int sampleIndex,
-                             SamplerHandle sampler, ScratchBuffer &scratchBuffer) final;
+    void EvaluatePixelSample(Point2i pPixel, int sampleIndex, SamplerHandle sampler,
+                             ScratchBuffer &scratchBuffer) final;
 
     virtual SampledSpectrum Li(RayDifferential ray, SampledWavelengths &lambda,
                                SamplerHandle sampler, ScratchBuffer &scratchBuffer,
@@ -315,8 +315,8 @@ class LightPathIntegrator : public ImageTileIntegrator {
     LightPathIntegrator(int maxDepth, CameraHandle camera, SamplerHandle sampler,
                         PrimitiveHandle aggregate, std::vector<LightHandle> lights);
 
-    void EvaluatePixelSample(const Point2i &pPixel, int sampleIndex,
-                             SamplerHandle sampler, ScratchBuffer &scratchBuffer);
+    void EvaluatePixelSample(Point2i pPixel, int sampleIndex, SamplerHandle sampler,
+                             ScratchBuffer &scratchBuffer);
 
     static std::unique_ptr<LightPathIntegrator> Create(
         const ParameterDictionary &parameters, CameraHandle camera, SamplerHandle sampler,

src/pbrt/options.h
@@ -23,6 +23,7 @@ struct BasicPBRTOptions {
     bool quiet = false;
     bool disablePixelJitter = false, disableWavelengthJitter = false;
     bool forceDiffuse = false;
+    bool useGPU = false;
     RenderingCoordinateSystem renderingSpace = RenderingCoordinateSystem::CameraWorld;
 };
 
@@ -30,7 +31,7 @@ struct BasicPBRTOptions {
 struct PBRTOptions : BasicPBRTOptions {
     int nThreads = 0;
     LogLevel logLevel = LogLevel::Error;
-    bool useGPU = false;
+    bool writePartialImages = false;
     bool recordPixelStatistics = false;
     pstd::optional<int> pixelSamples;
     pstd::optional<int> gpuDevice;

src/pbrt/util/sampling.h
@@ -263,8 +263,7 @@ inline Point2f InvertUniformSphereSample(const Vector3f &v) {
     return Point2f((1 - v.z) / 2, phi / (2 * Pi));
 }
 
-PBRT_CPU_GPU
-inline Point2f SampleUniformDiskPolar(const Point2f &u) {
+PBRT_CPU_GPU inline Point2f SampleUniformDiskPolar(const Point2f &u) {
     Float r = std::sqrt(u[0]);
     Float theta = 2 * Pi * u[1];
     return {r * std::cos(theta), r * std::sin(theta)};

commit 41f36d17172e89dfa32ae5eee3b51412dfa7c32c

Fix bug introduced in b459ba889bf727ce392532645213125b567d7fb7
Final images were never being written(!)

src/pbrt/cpu/integrators.cpp
@@ -243,7 +243,7 @@ void ImageTileIntegrator::Render() {
             nextWaveSize = std::min(2 * nextWaveSize, 64);
 
         // Optionally write current image to disk
-        if (Options->writePartialImages || referenceImage) {
+        if (waveStart == spp || Options->writePartialImages || referenceImage) {
             LOG_VERBOSE("Writing image with spp = %d", waveStart);
             ImageMetadata metadata;
             metadata.renderTimeSeconds = progress.ElapsedSeconds();
@@ -258,7 +258,7 @@ void ImageTileIntegrator::Render() {
                 metadata.MSE = mse.Average();
                 fflush(mseOutFile);
             }
-            if (Options->writePartialImages) {
+            if (waveStart == spp || Options->writePartialImages) {
                 camera.InitMetadata(&metadata);
                 camera.GetFilm().WriteImage(metadata, 1.0f / waveStart);
             }