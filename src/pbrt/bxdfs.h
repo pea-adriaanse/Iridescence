pea-adriaanse/Iridescence

commit 0ff538cd2e87468252030918a16ea3d7275043f6

DielectricBxDF: include BxDFFlags::Specular in flags when eta==1

src/pbrt/bxdfs.h
@@ -175,7 +175,7 @@ class DielectricBxDF {
     PBRT_CPU_GPU
     BxDFFlags Flags() const {
         if (eta == 1)
-            return BxDFFlags::Transmission;
+            return BxDFFlags::Transmission | BxDFFlags::Specular;
         else
             return BxDFFlags::Reflection | BxDFFlags::Transmission |
                    (mfDistrib.EffectivelySmooth() ? BxDFFlags::Specular

commit 0d941e9c1960f757a417745fdf685e6822998600

Fix bug in DielectricBxDF::Flags() for eta==1.
We still need to consider the glossy case... (Bug introduced in
0ff538cd2e87468252030918a16ea3d7275043f6.)

src/pbrt/bxdfs.h
@@ -175,7 +175,9 @@ class DielectricBxDF {
     PBRT_CPU_GPU
     BxDFFlags Flags() const {
         if (eta == 1)
-            return BxDFFlags::Transmission | BxDFFlags::Specular;
+            return BxDFFlags::Transmission |
+                   (mfDistrib.EffectivelySmooth() ? BxDFFlags::Specular
+                                                  : BxDFFlags::Glossy);
         else
             return BxDFFlags::Reflection | BxDFFlags::Transmission |
                    (mfDistrib.EffectivelySmooth() ? BxDFFlags::Specular